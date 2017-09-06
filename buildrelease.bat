d061e111363b354c094bd75c4b4aa0715829e212
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-09-06 06:58:01 -0400

    Reorganized a bit

---
+++ buildrelease.bat
@@ -0,0 +1,44 @@
+﻿@echo off
+cd
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+
+
+set VERSIONFILE=KRASH.version
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+
+
+xcopy Source\Textures\KRASH*.png   GameData\KRASH\Textures /Y
+copy ..\MiniAVC.dll Gamedata\KRASH
+copy Source\bin\Release\KRASH.dll Gamedata\KRASH\Plugins
+copy KRASH.version Gamedata\KRASH\KRASH.version
+copy ..\README.md Gamedata\KRASH
+copy Source\README4Modders.txt  Gamedata\KRASH
+copy Source\KRASHWrapper.cs  Gamedata\KRASH
+copy Source\ChangeLog.txt Gamedata\KRASH
+copy Source\KRASH.cfg Gamedata\KRASH\PluginData
+copy Source\KRASHCustom.cfg Gamedata\KRASH\PluginData
+
+rem copy KRASHCustom.cfg ..\Gamedata\KRASH
+
+set FILE="%RELEASEDIR%\KRASH-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% Gamedata\KRASH
+
+pause
---
+++ deploy.bat
@@ -0,0 +1,39 @@
+﻿
+@ech off
+set H=R:\KSP_1.3.0_dev
+echo %H%
+
+set d=%H%
+if exist %d% goto one
+mkdir %d%
+:one
+set d=%H%\Gamedata
+if exist %d% goto two
+mkdir %d%
+:two
+set d=%H%\Gamedata\KRASH
+if exist %d% goto three
+mkdir %d%
+:three
+set d=%H%\Gamedata\KRASH\Plugins
+if exist %d% goto four
+mkdir %d%
+:four
+set d=%H%\Gamedata\KRASH\Textures
+if exist %d% goto five
+mkdir %d%
+:five
+set d=%H%\Gamedata\KRASH\PluginData
+if exist %d% goto six
+mkdir %d%
+:six
+
+
+
+xcopy Source\Textures\KRASH*.png   %H%\GameData\KRASH\Textures /Y
+copy ..\MiniAVC.dll %H%\Gamedata\KRASH
+copy Source\bin\Debug\KRASH.dll %H%\Gamedata\KRASH\Plugins
+copy KRASH.version %H%\Gamedata\KRASH\KRASH.version
+copy Source\KRASH.cfg %H%\Gamedata\KRASH\PluginData
+copy Source\KRASHCustom.cfg %H%\Gamedata\KRASH\PluginData
+
